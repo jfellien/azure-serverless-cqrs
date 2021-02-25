34a7b8c9fd27a6847293a737b8a20854cd170897
Janek <jan.fellien@devcrowd.de>
2021-02-25 22:04:07 +0100

    added broadcasting

--- src/domain/sales/devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler/OrderPizza.cs
+++ src/domain/sales/devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler/OrderPizza.cs
@@ -20,12 +20,10 @@ namespace devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler
     {
         [FunctionName("OrderPizza")]
         public static async Task<IActionResult> RunAsync(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "order")]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "order")]
             Order order,
-            [DomainEventStream("Sales", "Order")]
-            DomainEventStream eventStream,
-            [Broadcaster]
-            Broadcaster broadcaster,
+            [DomainEventStream("Sales", "Order")] DomainEventStream eventStream,
+            [Broadcaster] Broadcaster broadcaster,
             ILogger log)
         {
             /// Here is the place for business validation.
@@ -33,7 +31,7 @@ namespace devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler
 
             var orderId = Guid.NewGuid().ToString();
             var orderNumber = NextOrderNumber();
-            
+
             if (IsValid(order))
             {
                 var orderAccepted = new OrderAccepted
@@ -45,19 +43,19 @@ namespace devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler
                     Cheese = order.Cheese,
                     Amount = order.Amount,
                     CustomerId = order.CustomerId
-                };    
-                
+                };
+
                 // Put the Event into EventStream.
                 // EntityId is needed because the Stream doesn't know the Id and can't read from Event 
                 await eventStream.Append(orderAccepted, orderAccepted.OrderId);
-                
+
                 await broadcaster.SendInfo(new BroadcastMessageToReceiver()
                 {
                     From = "Pizza Factory - Order System",
                     To = order.RequestTraceId,
-                    MessageText = $"Order accepted (Order Number: { orderNumber })"
+                    MessageText = $"Order accepted (Order Number: {orderNumber})"
                 });
-                
+
                 return new OkObjectResult(new
                 {
                     orderNumber = orderAccepted.OrderNumber,
@@ -71,17 +69,33 @@ namespace devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler
                 {
                     OrderId = orderId,
                     OrderNumber = orderNumber,
-                    OrderContent = $"Paste: {order.Paste}, Tomatoes: {order.Tomatoes}, Cheese{order.Cheese}, Amount: {order.Amount}",
+                    OrderContent =
+                        $"Paste: {order.Paste}, Tomatoes: {order.Tomatoes}, Cheese{order.Cheese}, Amount: {order.Amount}",
                     CustomerId = order.CustomerId,
                     Reason = "[put the reason in here]"
                 };
-                
-                await broadcaster.SendWarning(new BroadcastMessageToReceiver()
+
+                try
                 {
-                    From = "Pizza Factory - Order System",
-                    To = order.RequestTraceId,
-                    MessageText = $"Order declined (Order Number: { orderNumber } ==> Reason : [pu the reason in here])"
-                });
+                    await broadcaster.SendWarning(new BroadcastMessageToReceiver()
+                    {
+                        From = "Pizza Factory - Order System",
+                        To = order.RequestTraceId,
+                        MessageText =
+                            $"Order declined (Order Number: {orderNumber} ==> Reason : [pu the reason in here])"
+                    });
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex.Message, ex);
+                    
+                    return new BadRequestObjectResult(new
+                    {
+                        errorCode = "ORDER_DECLINED_BUT_SERVER_ERROR",
+                        message = $"Order declined because of [put the reason in here]" + 
+                                  $"(Internal Server Error: {ex.Message}"
+                    });
+                }
 
                 return new BadRequestObjectResult(new
                 {
@@ -95,6 +109,7 @@ namespace devCrowd.ServerlessCQRS.Contexts.Sales.CommandHandler
         {
             return string.IsNullOrWhiteSpace(order.Paste) == false;
         }
+
         /// <summary>
         /// Sample Method to generate a new Order Number
         /// </summary>
